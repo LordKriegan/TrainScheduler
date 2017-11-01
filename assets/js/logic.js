eec5120b54de3b4ab05ff47cbd7c1c6b2666e8cd
Qamar Stationwala <YoPappy0@gmail.com>
2017-11-01 08:21:01 -0700

    setup basic html page as well as firebase

---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBAlNRW_CpZc9Ny3dTliDGPx9y9div9CBY",
+    authDomain: "trainschedule-11fd2.firebaseapp.com",
+    databaseURL: "https://trainschedule-11fd2.firebaseio.com",
+    projectId: "trainschedule-11fd2",
+    storageBucket: "",
+    messagingSenderId: "756964373366"
+  };
+  firebase.initializeApp(config);
