988f98a27ddf96b058a7a0ef0463736b89d4c49f
Qamar Stationwala <YoPappy0@gmail.com>
2017-11-18 07:40:15 -0800

    can add trains, displays next time, but minutes away is still buggy

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,70 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBAlNRW_CpZc9Ny3dTliDGPx9y9div9CBY",
-    authDomain: "trainschedule-11fd2.firebaseapp.com",
-    databaseURL: "https://trainschedule-11fd2.firebaseio.com",
-    projectId: "trainschedule-11fd2",
-    storageBucket: "",
-    messagingSenderId: "756964373366"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBAlNRW_CpZc9Ny3dTliDGPx9y9div9CBY",
+  authDomain: "trainschedule-11fd2.firebaseapp.com",
+  databaseURL: "https://trainschedule-11fd2.firebaseio.com",
+  projectId: "trainschedule-11fd2",
+  storageBucket: "",
+  messagingSenderId: "756964373366"
+};
+firebase.initializeApp(config);
+database = firebase.database();
+
+database.ref("trains").on("value", function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+   //Here you can access  childSnapshot.key
+   var key = childSnapshot.val();
+   var ftt = moment(key.ftt, ["HH:mm"]);
+   var nextArrival = moment()
+   while (nextArrival.isBefore(ftt)) {
+    nextArrival.add(key.freq, "m");
+   } 
+
+   var minAway = moment((nextArrival.toDate() - moment().toDate()), ["mm"]).minute();
+   nextArrival = nextArrival.hour() + ":" + nextArrival.minute();
+   $("#trainTable").append(`
+      <tr>
+        <td>${key.name}</td>
+        <td>${key.dest}</td>
+        <td>${key.freq}</td>
+        <td>${nextArrival}</td>
+        <td>${minAway}</td>
+      </tr>
+    `)
+});
+
+}, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
+
+window.onload = function (){
+  $("#submitBtn").on("click", function(event) {
+    event.preventDefault();
+    var tName = $("#name-input").val().trim();
+    var tDest = $("#dest-input").val().trim();
+    var tFtt = $("#ftt-input").val().trim();
+    var tFreq = $("#freq-input").val().trim();
+    if (tName === "") {
+      console.error("Error: Invalid name");
+      return;
+    }
+    if (tDest === "") {
+      console.error("Error: Invalid destination");
+      return;
+    }
+    if (/^([0-9]|[01]\d|2[0-3]):([0-5]\d)$/.test(tFtt) === false) {
+      console.error("Error: Invalid time");
+      return;
+    }
+    if (Number.isInteger(Number(tFreq)) === false) {
+      console.error("Error: Invalid train frequency");
+      return;
+    }
+    database.ref("trains").push({
+      name: tName,
+      dest: tDest,
+      ftt: tFtt,
+      freq: tFreq
+    });
+  });
+}
